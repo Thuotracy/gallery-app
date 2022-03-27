644764ac3dc12609a79f928c98fed1dbce8e4b40
tracy <tracyjacobs775@gmail.com>
2022-03-27 18:53:05 +0300

    add class models.py file

--- tracy/models.py
+++ tracy/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-# class Location(models.Model):
-#     name = models.CharField(max_length=30)
+Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
 
-#     def save_location(self):
-#         self.save()
+    def save_location(self):
+        self.save()
 
-#     def delete_location(self):
-#         self.delete()
+    def delete_location(self):
+        self.delete()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 class categories(models.Model):
     name = models.CharField(max_length=30)
