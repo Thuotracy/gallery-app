4c705241ec3dee637446efcd8f91f18cb419ab9e
tracy <tracyjacobs775@gmail.com>
2022-03-30 10:34:11 +0300

    final commit

--- gallery/settings.py
+++ gallery/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import cloudinary
 import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/tracy/Downloads/gallery/Virtual"
+set -gx VIRTUAL_ENV "/home/tracy/Documents/gallery/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(Virtual) "
-            printf "%s%s" "(Virtual) " (set_color normal)
+        if test -n "(virtual) "
+            printf "%s%s" "(virtual) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
